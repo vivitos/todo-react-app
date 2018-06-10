07b1c862141d7c2c3c9368270908cc8385c63502
Vivitos <vivien.brunel21@gmail.com>
2018-06-10 09:40:23 +0200

    package: Add react-debounce-input
    Todo: Add react debounce input on label

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { DebounceInput } from 'react-debounce-input';
 
 class Todo extends Component {
 	render() {
 		return (
 			<li className="pt-2">
 				<input type="checkbox" className="checkbox" checked={this.props.todo.done} onChange={(event) => this.props.onChange(event, this.props.index, this.props.todo)}></input>
-				<input className="class-form mx-2" type="text" value={this.props.todo.label} onChange={(event) => this.props.onChange(event, this.props.index, this.props.todo)}></input>
+				<DebounceInput debounceTimeout={400} minLength={0} className="class-form mx-2" value={this.props.todo.label} onChange={(event) => this.props.onChange(event, this.props.index, this.props.todo)}></DebounceInput>
 				<a href="" className="No-decoration" onClick={(event) => { this.props.deleteTodo(event, this.props.index) }}>X</a>
 			</li>
 		)
