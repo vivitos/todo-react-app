ff8b92217f17a5cd56d81dee6059a7526b8202f8
Vivitos <vivien.brunel21@gmail.com>
2018-06-10 09:06:46 +0200

    package: Add bootstrap
    App: Add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import List from './components/List';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
